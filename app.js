e63c5c7b14103fec392287e2412aad58e093b1b1
Advali Pty Ltd <bimalkprabha24@gmail.com>
2020-08-19 16:11:54 +0800

    Portfolio-1
    Elena Joy -Photography Website

---
+++ app.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector(".nav-links");
+const burger = document.querySelector(".burger");
+const links = nav.querySelectorAll("a");
+
+burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    // when clicked add class list 
+    burger.classList.toggle("toggle");
+})
+
+links.forEach(links => {
+    links.addEventListener('click', () => {
+        nav.classList.toggle('nav-open');
+        burger.classList.toggle("toggle");
+    })
+})
